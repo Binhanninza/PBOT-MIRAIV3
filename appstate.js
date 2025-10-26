1189415cfe79bfb7f58456b4459e740d934a2cec
Binhanninza <143570394+Binhanninza@users.noreply.github.com>
2025-10-26 11:35:39 +0700

    Add Facebook login cookie data to appstate.js
    Added an array of cookie objects for Facebook login.

--- appstate.js
+++ appstate.js
@@ -1,1 +1,137 @@
-//thích thì add appstate vô để cho nó chạy command
+[
+    {
+        "key": "dbln",
+        "value": "%7B%2261576113710094%22%3A%22sEadFTWl%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.624Z",
+        "lastAccessed": "2025-10-26T03:42:32.628Z"
+    },
+    {
+        "key": "datr",
+        "value": "YZX9aGFcjyKaUfketK2TuZF0",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.628Z",
+        "lastAccessed": "2025-10-26T03:42:32.628Z"
+    },
+    {
+        "key": "sb",
+        "value": "YZX9aIQ5DbJtHxfxiTOIHolZ",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.628Z",
+        "lastAccessed": "2025-10-26T03:42:32.628Z"
+    },
+    {
+        "key": "m_pixel_ratio",
+        "value": "3",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.628Z",
+        "lastAccessed": "2025-10-26T03:42:32.628Z"
+    },
+    {
+        "key": "wd",
+        "value": "360x803",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.628Z",
+        "lastAccessed": "2025-10-26T03:42:32.628Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.628Z",
+        "lastAccessed": "2025-10-26T03:42:32.628Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.628Z",
+        "lastAccessed": "2025-10-26T03:42:32.628Z"
+    },
+    {
+        "key": "vpd",
+        "value": "v1%3B675x360x3",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.628Z",
+        "lastAccessed": "2025-10-26T03:42:32.628Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61576113710094",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.628Z",
+        "lastAccessed": "2025-10-26T03:42:32.628Z"
+    },
+    {
+        "key": "xs",
+        "value": "50%3Aft9HUMfXlLG39g%3A2%3A1761450047%3A-1%3A-1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.629Z",
+        "lastAccessed": "2025-10-26T03:42:32.629Z"
+    },
+    {
+        "key": "locale",
+        "value": "ja_JP",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.629Z",
+        "lastAccessed": "2025-10-26T03:42:32.629Z"
+    },
+    {
+        "key": "fr",
+        "value": "05X9zeedoGzmrIo8N.AWerguCV814OBZdY6_1vypFi5Zhk8yKCnTAZIbaVg4tbTWzxHHY.Bo_ZVh..AAA.0.0.Bo_ZhR.AWfA5koEq8RUWnJ6MtvWK458pWE",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.629Z",
+        "lastAccessed": "2025-10-26T03:42:32.629Z"
+    },
+    {
+        "key": "pas",
+        "value": "100095329730145%3A4SIXZHScQC%2C61576113710094%3AUAFqfHUTKM",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.629Z",
+        "lastAccessed": "2025-10-26T03:42:32.629Z"
+    },
+    {
+        "key": "fbl_st",
+        "value": "101524294%3BT%3A29357501",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.629Z",
+        "lastAccessed": "2025-10-26T03:42:32.629Z"
+    },
+    {
+        "key": "wl_cbv",
+        "value": "v2%3Bclient_version%3A2965%3Btimestamp%3A1761450065",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-26T03:42:32.629Z",
+        "lastAccessed": "2025-10-26T03:42:32.629Z"
+    }
+]
